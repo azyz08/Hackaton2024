d34e550e22f144cca2fbbdd758953685a906acc8
azyz08 <azibekibrohimova014@gmail.com>
2024-08-06 16:01:59 +0500

    dvsdsvsb

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,25 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Murojaat from "./pages/murojaat";
 import Contact from "./pages/contact";
-import Footer from "./components/footer/footer";
+// import Footer from "./components/footer";
 
 
 export default function App() {
   const { darkMode } = useMode();
   return (
     <>
-    <div className={`${darkMode ? "dark" : ""}`}>
-      <Navbar />
-      <div className="bigBox duration-300 dark:bg-[#121212] text-[white]">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/murojaat" element={<Murojaat />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+      <div className={`${darkMode ? "dark" : ""}`}>
+        <Navbar />
+        <div className="bigBox duration-300 dark:bg-[#121212] text-[white]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/murojaat" element={<Murojaat />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-    <Footer/>
+      {/* <Footer /> */}
     </>
   );
 }
